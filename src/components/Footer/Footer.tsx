9ed0b633a627669c81c9a30c954cc3cb77cddaa9
pablobeckg <beckgarreaud@gmail.com>
2024-09-04 09:59:16 +0200

    style footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,37 @@
-import { Link } from "react-router-dom";
-import "./Footer.css"
+import { Link, NavLink } from "react-router-dom";
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <div className="footer-container">
-            <div className="packageContainer">
-                <div className="logoPackage">
-                    <img src="/compass.png" alt="exploreLogo" />
-                    <Link to='/'>Explore</Link>
-                </div>
-                <div className="logoPackage">
-                    <img src="/Calender.png" alt="eventsLogo" />
-                    <Link to='/liked'>Events</Link>
-                </div>
-                <div className="logoPackage">
-                    <img src="/search.png" alt="searchLogo" />
-                    <Link to='/search'>Search</Link>
-                </div>
-                <div className="logoPackage">
-                    <img src="/profilLogo.png" alt="profilLogo" />
-                <Link to='/profile'>Profile</Link>
-                </div>
-            </div>
+  return (
+    <div className="footer-container">
+      <NavLink className="add-event-button" to="addevent">
+        <img src="/Plus.png" alt="Add event logo" />
+      </NavLink>
+
+      <div className="buttons-container">
+        <div className="logoPackage">
+          <NavLink to="/">
+            <img src="/Explore.png" alt="eventsLogo" />
+          </NavLink>
         </div>
-    );
-}
- 
+        <div className="logoPackage">
+          <NavLink to="/liked">
+            <img src="/Events.png" alt="eventsLogo" />
+          </NavLink>
+        </div>
+        <div className="logoPackage">
+          <NavLink to="/search">
+            <img src="/Search.png" alt="searchLogo" />
+          </NavLink>
+        </div>
+        <div className="logoPackage">
+          <NavLink to="/profile">
+            <img src="/Profile-footer.png" alt="profilLogo" />
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Footer;
