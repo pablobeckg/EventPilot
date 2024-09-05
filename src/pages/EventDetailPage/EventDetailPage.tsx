eb07df41bd9cf60fe40e42bb5c2fef46165cad00
noctwo <mr.evok@gmail.com>
2024-09-05 12:02:46 +0200

    finetuning eventdetail

--- src/pages/EventDetailPage/EventDetailPage.tsx
+++ src/pages/EventDetailPage/EventDetailPage.tsx
@@ -3,15 +3,21 @@ import { useParams } from "react-router-dom";
 import { EventComplete } from "../../types/supabase-types-own";
 import supabaseClient from "../../lib/supabaseClient";
 import "./EventDetailPage.css";
+import FavoriteIcon from "../../assets/svg/FavoriteIcon";
+import UnfavoriteIcon from "../../assets/svg/UnfavoriteIcon";
+import { useUserContext } from "../../context/UserContext";
 
 const EventDetailPage = () => {
   const { id } = useParams<{ id: string }>();
   const [event, setEvent] = useState<EventComplete | null>(null);
+  const [events, setEvents] = useState<EventComplete[]>([]);
+  const userContext = useUserContext();
+  const user = userContext?.user;
 
   useEffect(() => {
     const fetchSingleEvent = async () => {
       if (!id) {
-        console.error("No quiz id given.");
+        console.error("No event id given.");
         return;
       }
       let selectQuery = supabaseClient
@@ -31,8 +37,77 @@ const EventDetailPage = () => {
     fetchSingleEvent();
   }, []);
 
+
+  const addFavorite = async (eventId: string) => {
+    const favoritesInsertResponse = await supabaseClient
+      .from("favorites")
+      .insert({ user_id: user?.id, event_id: eventId });
+
+    if (favoritesInsertResponse.error) {
+      console.error(
+        "Error setting favorite",
+        favoritesInsertResponse.error.message
+      );
+      return;
+    } else {
+      setEvents(
+        events.map((event) => {
+          if (event.id === eventId) {
+            const updatedFavorites = [
+              ...(event.favorites ?? []),
+              { event_id: eventId, user_id: user!.id, id: "", created_at: "" },
+            ];
+            return { ...event, favorites: updatedFavorites };
+          }
+          return event;
+        })
+      );
+    }
+  };
+  const deleteFavorite = async (eventId: string) => {
+    const favoritesDeleteResponse = await supabaseClient
+      .from("favorites")
+      .delete()
+      .eq("event_id", eventId)
+      .eq("user_id", user!.id);
+
+    if (favoritesDeleteResponse.error) {
+      console.error("Error deleting favorite", favoritesDeleteResponse.error);
+    } else {
+      setEvents(
+        events.map((event) => {
+          const newFavoritesWithoutEvent = (event.favorites ?? []).filter(
+            (fav) => fav.event_id !== eventId
+          );
+          return event.id === eventId
+            ? { ...event, favorites: newFavoritesWithoutEvent }
+            : event;
+        })
+      );
+    }
+  };
+
   return (
     <main className="event-detail-container">
+      <div className="event-favorite-icon">
+                <button
+                  onClick={() =>
+                    event?.favorites?.find(
+                      (favorite) => favorite.event_id === event!.id
+                    )
+                      ? deleteFavorite(event!.id)
+                      : addFavorite(event!.id)
+                  }
+                >
+                  {event?.favorites?.find(
+                    (favorite) => favorite.event_id === event.id
+                  ) ? (
+                    <FavoriteIcon />
+                  ) : (
+                    <UnfavoriteIcon />
+                  )}
+                </button>
+              </div>
       <div className="single-event-img-container">
       <img src={event?.event_image} alt="EventImgPlaceholder" />
       </div>
