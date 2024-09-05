c56f090244aacac9918276c5767540b32469eb7b
pablobeckg <beckgarreaud@gmail.com>
2024-09-05 10:51:26 +0200

    finished Liked Events

--- src/pages/LikedPage/LikedPage.tsx
+++ src/pages/LikedPage/LikedPage.tsx
@@ -3,94 +3,103 @@ import { EventComplete } from "../../types/supabase-types-own";
 import { useUserContext } from "../../context/UserContext";
 import supabaseClient from "../../lib/supabaseClient";
 import FavoriteIcon from "../../assets/svg/FavoriteIcon";
+import "./LikedPage.css";
+import formatEventDate from "../../services/formatEventDate";
 
 const LikedPage = () => {
-    const [events, setEvents] = useState<EventComplete[]>([]);
-    const [likedEventIds, setLikedEventIds] = useState<string[]>([]);
-    const userContext = useUserContext();
-    const user = userContext?.user;
+  const [events, setEvents] = useState<EventComplete[]>([]);
+  const [likedEventIds, setLikedEventIds] = useState<string[]>([]);
+  const userContext = useUserContext();
+  const user = userContext?.user;
   
-    if (!user) {
-        return;
-      }
-  
-    useEffect(() => {
-        
-      const fetchLikedEvents = async () => {
-        let selectQuery = supabaseClient
-          .from("events")
-          .select("*, categories(*), locations(*), venues(*), favorites(*)")
-          .eq("favorites.user_id", user.id);
-  
-        const result = await selectQuery;
-        if (result.error) {
-          setEvents([]);
-        } else {
-          setEvents(result.data);
-        }
 
-        const selectLiked = supabaseClient
+  if (!user) {
+    return;
+  }
+
+  useEffect(() => {
+    const fetchLikedEvents = async () => {
+      let selectQuery = supabaseClient
+        .from("events")
+        .select("*, categories(*), locations(*), venues(*), favorites(*)")
+        .eq("favorites.user_id", user.id);
+
+      const result = await selectQuery;
+      if (result.error) {
+        setEvents([]);
+      } else {
+        setEvents(result.data);
+      }
+
+      const selectLiked = supabaseClient
         .from("favorites")
         .select("*")
         .eq("user_id", user.id);
 
-        const resultLiked = await selectLiked;
+      const resultLiked = await selectLiked;
 
-        if (resultLiked.error) {
-          setEvents([]);
-          } else {
-            const likedIds = resultLiked.data.map(
-              (liked: { event_id: string }) => liked.event_id
-            );
-            setLikedEventIds(likedIds);
-          }
+      if (resultLiked.error) {
+        setEvents([]);
+      } else {
+        const likedIds = resultLiked.data.map(
+          (liked: { event_id: string }) => liked.event_id
+        );
+        setLikedEventIds(likedIds);
+      }
+    };
+    fetchLikedEvents();
+  }, [user.id]);
 
-      };
-      fetchLikedEvents();
-    }, [user.id]);
+  const favoriteEvents = events.filter((event) =>
+    likedEventIds.includes(event.id)
+  );
 
-    const favoriteEvents = events.filter((event) =>
-        likedEventIds.includes(event.id)
-      );
+  const deleteFavorite = async (eventId: string) => {
+    const favoritesDeleteResponse = await supabaseClient
+      .from("favorites")
+      .delete()
+      .eq("event_id", eventId)
+      .eq("user_id", user.id);
 
-      
-      const deleteFavorite = async (eventId: string) => {
-        const favoritesDeleteResponse = await supabaseClient
-          .from("favorites")
-          .delete()
-          .eq("event_id", eventId)
-          .eq("user_id", user.id);
-      
-        if (favoritesDeleteResponse.error) {
-          console.error("Error deleting favorite", favoritesDeleteResponse.error);
-        } else {
-          setEvents(events.filter((event) => event.id !== eventId));
-          
-          setLikedEventIds(likedEventIds.filter((id) => id !== eventId));
-        }
-      };
-      
+    if (favoritesDeleteResponse.error) {
+      console.error("Error deleting favorite", favoritesDeleteResponse.error);
+    } else {
+      setEvents(events.filter((event) => event.id !== eventId));
 
-    return (
-        
-      <div>
+      setLikedEventIds(likedEventIds.filter((id) => id !== eventId));
+    }
+  };
+
+  return (
+    <div className="liked-page-container">
+      <h1>Liked Events</h1>
       {events.length === 0 ? (
         <p>No favorite events found.</p>
       ) : (
         favoriteEvents.map((event) => (
-          <div key={event.id}>
-            <h1>{event.event_title}</h1>
-            <div className="event-favorite-icon">
-              <button onClick={() => deleteFavorite(event.id)}>
-                <FavoriteIcon />
-              </button>
+          <div className="event-item-style" key={event.id}>
+            <img src={event.event_image} alt="" />
+            <div className="information-container">
+              <div>
+                <div className="event-favorite-top">
+                  <h3>{formatEventDate(`${event.event_date} ${event.event_start_time}`)}</h3>
+                  <button onClick={() => deleteFavorite(event.id)}>
+                    <FavoriteIcon />
+                  </button>
+                </div>
+                <h2>{event.event_title}</h2>
+              </div>
+
+              <div className="event-location">
+                <img src="/Location.png" alt="" />
+                <h3>{event.locations?.location_name}</h3>
+              </div>
             </div>
           </div>
         ))
       )}
     </div>
-    );
-}
- 
-export default LikedPage;
+  );
+};
 
+export default LikedPage;
---
+++ src/services/formatEventDate.tsx
@@ -0,0 +1,31 @@
+function formatEventDate(dateString: string) {
+    const date = new Date(dateString);
+  
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "short" });
+    const dayOfWeek = date.toLocaleString("default", { weekday: "short" });
+  
+    const getOrdinalSuffix = (n: number) => {
+      if (n > 3 && n < 21) return "th";
+      switch (n % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    };
+  
+    const hour = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const ampm = date.getHours() >= 12 ? "PM" : "AM";
+  
+    const formattedDate = `${day}${getOrdinalSuffix(day)} ${month} - ${dayOfWeek.toUpperCase()} - ${hour}:${minutes} ${ampm}`;
+    
+    return formattedDate.toLocaleUpperCase();
+  }
+
+  export default formatEventDate
