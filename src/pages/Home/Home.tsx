92d53702171c76e96ee2593e623eb54bdde79bc1
pablobeckg <beckgarreaud@gmail.com>
2024-09-05 09:47:47 +0200

    fixed

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,62 +1,57 @@
 import { useEffect, useState } from "react";
 import supabaseClient from "../../lib/supabaseClient";
 import { Link } from "react-router-dom";
-import "./Home.css"
-import {
-  EventComplete,
-  Location,
-} from "../../types/supabase-types-own";
+import "./Home.css";
+import { EventComplete, Location } from "../../types/supabase-types-own";
 import { useUserContext } from "../../context/UserContext";
 import FavoriteIcon from "../../assets/svg/FavoriteIcon";
 import UnfavoriteIcon from "../../assets/svg/UnfavoriteIcon";
 
 const Home = () => {
-    const [upcomingEvents, setupcomingEvents] = useState<EventComplete[]>([]);
-    const [nearby, setNearby] = useState<EventComplete[]>([]);
-    const [locations, setLocations] = useState<Location[]>([]);
-    const [selectedLocation, setSelectedLocation] = useState<string | null>(null);
-    const [events, setEvents] = useState<EventComplete[]>([]);
-    const userContext = useUserContext();
-    const user = userContext?.user;
+  const [upcomingEvents, setupcomingEvents] = useState<EventComplete[]>([]);
+  const [nearby, setNearby] = useState<EventComplete[]>([]);
+  const [locations, setLocations] = useState<Location[]>([]);
+  const [selectedLocation, setSelectedLocation] = useState<string | null>(null);
+  const [events, setEvents] = useState<EventComplete[]>([]);
+  const userContext = useUserContext();
+  const user = userContext?.user;
 
-    if (!user) {
-      return;
-    }
+  if (!user) {
+    return;
+  }
 
- 
-    useEffect(() => {
-      const fetchEvents = async () => {
-        let selectQuery = supabaseClient
-          .from("events")
-          .select("*, categories(*), locations(*), venues(*), favorites(*)")
-          .order("event_date", { ascending: true });
-    
-        const result = await selectQuery;
-        if (result.error) {
-          setupcomingEvents([]);
-        } else {
-          setupcomingEvents(result.data);
-        }
-    
-        let nearbyQuery = supabaseClient
-          .from("events")
-          .select("*, categories(*), locations(*), venues(*), favorites(*)");
-    
-        if (selectedLocation) {
-          nearbyQuery = nearbyQuery.eq("location_id", selectedLocation);
-        }
-    
-        const resultNearby = await nearbyQuery;
-        if (resultNearby.error) {
-          setNearby([]);
-        } else {
-          setNearby(resultNearby.data);
-        }
-      };
-    
-      fetchEvents();
-    }, [selectedLocation]);
-    
+  useEffect(() => {
+    const fetchEvents = async () => {
+      let selectQuery = supabaseClient
+        .from("events")
+        .select("*, categories(*), locations(*), venues(*), favorites(*)")
+        .order("event_date", { ascending: true });
+
+      const result = await selectQuery;
+      if (result.error) {
+        setupcomingEvents([]);
+      } else {
+        setupcomingEvents(result.data);
+      }
+
+      let nearbyQuery = supabaseClient
+        .from("events")
+        .select("*, categories(*), locations(*), venues(*), favorites(*)");
+
+      if (selectedLocation) {
+        nearbyQuery = nearbyQuery.eq("location_id", selectedLocation);
+      }
+
+      const resultNearby = await nearbyQuery;
+      if (resultNearby.error) {
+        setNearby([]);
+      } else {
+        setNearby(resultNearby.data);
+      }
+    };
+
+    fetchEvents();
+  }, [selectedLocation]);
 
   useEffect(() => {
     const fetchLocations = async () => {
@@ -143,116 +138,140 @@ const Home = () => {
 
   return (
     <>
-    <div className="eventBase">
-      <div className="eventTop">
-        <div className="logo">
+      <div className="eventBase">
+        <div className="eventTop">
+          <div className="logo">
             <img src="/Logo.png" alt="Logo" />
+          </div>
+          <select
+            className="locationSelection"
+            name="location"
+            id="location-select"
+            value={selectedLocation || ""}
+            onChange={(e) => setSelectedLocation(e.target.value)}
+          >
+            <option value="">All Locations</option>
+            {locations.map((location) => (
+              <option key={location.id} value={location.id}>
+                {location.location_name}
+              </option>
+            ))}
+          </select>
         </div>
-        <select className="locationSelection"
-              name="location"
-              id="location-select"
-              value={selectedLocation || ""}
-              onChange={(e) => setSelectedLocation(e.target.value)}
-            >
-              <option value="">All Locations</option>
-              {locations.map((location) => (
-                <option key={location.id} value={location.id}>
-                  {location.location_name}
-                </option>
-              ))}
-        </select>
-      </div>
-  <main className="event-list-container">
-    <div>
-      <h2 className="upcomingEventTitel">Upcoming Events</h2>
-    </div>
-    <div className="scrollEvents">
-      {upcomingEvents.length === 0 && <p>No Events yet</p>}
-      {upcomingEvents && upcomingEvents.length > 0 && upcomingEvents.map((event) => (
-          
-          <article className="event-item-container" key={event.id}>
-            <img src={event.event_image} alt="EventImgPlaceholder" />
-            <div className="event-item-contaier-item">
-              <Link to={`event/${event.id}`}>
-              <h2>{event.event_date} {event.event_start_time}</h2>
-              <h1>{event.event_title}</h1>
-              <div className="locationContainer"><img src="/Registered.png" alt="registeredImg" /><img className="mapPin" src="/MapPin.png" alt="mapPin" /><h2>{event.locations?.location_name}</h2></div>
-              </Link>
-            </div>
-          </article>
-        
-      ))}
-    </div>
-    <div className="nearbyContainer">
-      <h2 className="nearbyEventTitel">Nearby You</h2>
-      <p>See All</p>
-    </div>
-    <div className="scrollEvents">
-      {nearby.length === 0 && <p>No Events yet</p>}
-          {nearby && nearby.length > 0 && nearby.map((event) => (
-              
-              <article className="event-item-container" key={event.id}>
-                <img src={event.event_image} alt="EventImgPlaceholder" />
-                <div className="event-item-contaier-item">
-                  <Link to={`event/${event.id}`}>
-                  <h2>{event.event_date} {event.event_start_time}</h2>
-                  <h1>{event.event_title}</h1>
-                  <div className="locationContainer"><img src="/Registered.png" alt="registeredImg" /><img className="mapPin" src="/MapPin.png" alt="mapPin" /><h2>{event.locations?.location_name}</h2></div>
-                  </Link>
-                </div>
-              </article>
-            
-          ))}
-    </div>
-    <div className="allEventsTitel">
-      <h2>all Events</h2>
-    </div>
-    <div className="scrollAllEvents">
-        {events.length === 0 && <p>No Events yet</p>}
-        {events &&
-          events.length > 0 &&
-          events.map((event) => (
-            <article className="all-event-item-container" key={event.id}>
-              <Link to={`event/${event.id}`}>
-                <img src={event.event_image} alt="" />
-                <div className="eventInfo">
-                  <h2>
-                    {event.event_date} {event.event_start_time}
-                  </h2>
-                  <h1>{event.event_title}</h1>
-                  <div className="locationContainer">
-                    <img className="mapPin" src="/MapPin.png" alt="mapPin" />
-                    <h2>{event.locations?.location_name}</h2>
+        <main className="event-list-container">
+          <div>
+            <h2 className="upcomingEventTitel">Upcoming Events</h2>
+          </div>
+          <div className="scrollEvents">
+            {upcomingEvents.length === 0 && <p>No Events yet</p>}
+            {upcomingEvents &&
+              upcomingEvents.length > 0 &&
+              upcomingEvents.map((event) => (
+                <article className="event-item-container" key={event.id}>
+                  <img src={event.event_image} alt="EventImgPlaceholder" />
+                  <div className="event-item-contaier-item">
+                    <Link to={`event/${event.id}`}>
+                      <h2>
+                        {event.event_date} {event.event_start_time}
+                      </h2>
+                      <h1>{event.event_title}</h1>
+                      <div className="locationContainer">
+                        <img src="/Registered.png" alt="registeredImg" />
+                        <img
+                          className="mapPin"
+                          src="/MapPin.png"
+                          alt="mapPin"
+                        />
+                        <h2>{event.locations?.location_name}</h2>
+                      </div>
+                    </Link>
                   </div>
-                </div>
-              </Link>
-              <div className="event-favorite-icon">
-                <button
-                  onClick={() =>
-                    event.favorites?.find(
-                      (favorite) => favorite.event_id === event.id
-                    )
-                      ? deleteFavorite(event.id)
-                      : addFavorite(event.id)
-                  }
-                >
-                  {event.favorites?.find(
-                    (favorite) => favorite.event_id === event.id
-                  ) ? (
-                    <FavoriteIcon />
-                  ) : (
-                    <UnfavoriteIcon />
-                  )}
-                </button>
-              </div>
-            </article>
-          ))}
-    </div>
-
-  </main>
-  </div>
-  </>
-);
+                </article>
+              ))}
+          </div>
+          <div className="nearbyContainer">
+            <h2 className="nearbyEventTitel">Nearby You</h2>
+            <p>See All</p>
+          </div>
+          <div className="scrollEvents">
+            {nearby.length === 0 && <p>No Events yet</p>}
+            {nearby &&
+              nearby.length > 0 &&
+              nearby.map((event) => (
+                <article className="event-item-container" key={event.id}>
+                  <img src={event.event_image} alt="EventImgPlaceholder" />
+                  <div className="event-item-contaier-item">
+                    <Link to={`event/${event.id}`}>
+                      <h2>
+                        {event.event_date} {event.event_start_time}
+                      </h2>
+                      <h1>{event.event_title}</h1>
+                      <div className="locationContainer">
+                        <img src="/Registered.png" alt="registeredImg" />
+                        <img
+                          className="mapPin"
+                          src="/MapPin.png"
+                          alt="mapPin"
+                        />
+                        <h2>{event.locations?.location_name}</h2>
+                      </div>
+                    </Link>
+                  </div>
+                </article>
+              ))}
+          </div>
+          <div className="allEventsTitel">
+            <h2>all Events</h2>
+          </div>
+          <div className="scrollAllEvents">
+            {events.length === 0 && <p>No Events yet</p>}
+            {events &&
+              events.length > 0 &&
+              events.map((event) => (
+                <article className="all-event-item-container" key={event.id}>
+                  <Link to={`event/${event.id}`}>
+                    <img src={event.event_image} alt="" />
+                    <div className="eventInfo">
+                      <h2>
+                        {event.event_date} {event.event_start_time}
+                      </h2>
+                      <h1>{event.event_title}</h1>
+                      <div className="locationContainer">
+                        <img
+                          className="mapPin"
+                          src="/MapPin.png"
+                          alt="mapPin"
+                        />
+                        <h2>{event.locations?.location_name}</h2>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className="event-favorite-icon">
+                    <button
+                      onClick={() =>
+                        event.favorites?.find(
+                          (favorite) => favorite.event_id === event.id
+                        )
+                          ? deleteFavorite(event.id)
+                          : addFavorite(event.id)
+                      }
+                    >
+                      {event.favorites?.find(
+                        (favorite) => favorite.event_id === event.id
+                      ) ? (
+                        <FavoriteIcon />
+                      ) : (
+                        <UnfavoriteIcon />
+                      )}
+                    </button>
+                  </div>
+                </article>
+              ))}
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Home;
